b4f313dea70bf21e82a371d8aa961c78e91c85f0
Ricky Aponte <Rickyapontephotography@gmail.com>
2022-11-16 20:14:30 -0500

    completed the employee test JS

---
+++ tests/Employee.test.js
@@ -0,0 +1,43 @@
+const Employee = require("../lib/Employee");
+
+describe('Employee Class', () => {
+    describe('Employee class', () => {
+        it('should create an object with a name, id and email if provided valid arguments', () => {
+            const emp = new Employee("Rick", 16, "Ra@gmail.com");
+      
+            expect(emp.name).toEqual("Rick");
+            expect(emp.id).toEqual(16);
+            expect(emp.email).toEqual("Ra@gmail.com");
+        });
+  
+      // GetName test
+        it("should return name from getName()", () => {
+            const testName = "Ricky"
+            const emp = new Employee(testName)
+
+            expect(emp.getName()).toBe(testName);
+      });
+      // GetID test
+        it("should return id from getId()", () => {
+            const testId = 20
+            const emp = new Employee("rick", testId)
+
+            expect(emp.getId()).toBe(testId);
+    });
+      // GetEmail test
+        it("should return email from getEmail()", () => {
+            const testEmail = "Ra@gmail.com"
+            const emp = new Employee("rick", 15, testEmail)
+
+            expect(emp.getEmail()).toBe(testEmail);
+    });
+      // GetRole test
+        it("should return Role from getRole()", () => {
+            const testVal = "Employee"
+            const emp = new Employee("rick", 15, "test@test.com")
+
+            expect(emp.getRole()).toBe(testVal);
+    });
+    });
+  });
+
