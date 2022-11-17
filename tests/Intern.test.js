e9d8c23b6152436128f004dff629e10175a38139
Ricky Aponte <Rickyapontephotography@gmail.com>
2022-11-16 20:36:40 -0500

    Completed all tests and they all pass

---
+++ tests/Intern.test.js
@@ -0,0 +1,27 @@
+const Intern = require("../lib/Intern");
+
+describe('Intern Class', () => {
+    describe('Intern class', () => {
+        it('Should return the School from constructor', () => {
+            const testSchool = "Uconn";
+            const int = new Intern("Rick", 16, "Ra@gmail.com", testSchool);
+      
+            expect(int.school).toEqual(testSchool);
+        });
+  
+      // GetSchool test
+        it("should return school name from getSchool()", () => {
+            const testSchool = "Uconn";
+            const int = new Intern("Rick", 16, "Ra@gmail.com", testSchool)
+
+            expect(int.getSchool()).toBe(testSchool);
+      });
+      // GetRole test
+        it("should return role from getRole()", () => {
+            const testVal = "Intern";
+            const int = new Intern("Rick", 16, "Ra@gmail.com", "Eaponte2")
+
+            expect(int.getRole()).toBe(testVal);
+     }); 
+    });
+  });
---
+++ tests/Manager.test.js
@@ -0,0 +1,28 @@
+const Manager = require("../lib/Manager");
+const Employee = require("../lib/Employee");
+
+describe('Manager Class', () => {
+    describe('Manager class', () => {
+        it('Should return the OfficeNumber from constructor', () => {
+            const testOfficNum = 13;
+            const man = new Manager("Rick", 16, "Ra@gmail.com", testOfficNum);
+      
+            expect(man.officeNumber).toEqual(testOfficNum);
+        });
+  
+      // GetOfficerNumber() test
+        it("should return office number from getOfficeNumber()", () => {
+            const testOfficNum = 13;
+            const man = new Manager("Rick", 16, "Ra@gmail.com", testOfficNum)
+
+            expect(man.getOfficeNumber()).toBe(testOfficNum);
+      });
+      // GetRole() test
+        it("should return role from getRole()", () => {
+            const testVal = "Manager";
+            const man = new Manager("Rick", 16, "Ra@gmail.com", "Eaponte2")
+
+            expect(man.getRole()).toBe(testVal);
+     }); 
+    });
+  });
